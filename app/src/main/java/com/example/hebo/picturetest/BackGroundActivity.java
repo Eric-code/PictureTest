a2f8f46e66074d006f0038bc740c06564916d826
Hebo <hebo@bupt.edu.cn>
2017-07-04 16:36:46 +0800

    初步完成背景图片界面，可以选择图库图片和照相调用，仍未实现菜单栏动态加减

---
+++ app/src/main/java/com/example/hebo/picturetest/BackGroundActivity.java
@@ -0,0 +1,210 @@
+package com.example.hebo.picturetest;
+
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.jar.Manifest;
+
+public class BackGroundActivity extends AppCompatActivity {
+    private SearchView mSearchView;
+    PopupMenu popupMenu;
+    Menu menu;
+    public static final int TAKE_PHOTO=1;//照相
+    public static final int CHOOSE_PHOTO=2;//图库中选择照片
+    private ImageView picture;
+    private Uri imageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back_ground);
+        picture=(ImageView)findViewById(R.id.picture);
+        mSearchView = (SearchView) findViewById(R.id.searchView);
+        mSearchView.onActionViewExpanded();// 写上此句后searchView初始是可以点击输入的状态
+        // 设置搜索文本监听
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // 当点击搜索按钮时触发该方法
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            // 当搜索内容改变时触发该方法
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (!TextUtils.isEmpty(newText)){
+                    //mListView.setFilterText(newText);
+                    Toast.makeText(BackGroundActivity.this,"delete",Toast.LENGTH_SHORT).show();
+                }else{
+                    //mListView.clearTextFilter();
+                    Toast.makeText(BackGroundActivity.this,"delete",Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
+        popupMenu = new PopupMenu(this, findViewById(R.id.popupmenu_btn));
+        menu = popupMenu.getMenu();
+        // 通过XML文件添加菜单项
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.popupmenu, menu);
+        // 监听事件
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.choose_from_album:
+                        if (ContextCompat.checkSelfPermission(BackGroundActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                            ActivityCompat.requestPermissions(BackGroundActivity.this,new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                        }else {
+                            openAlbum();
+                        }
+                        break;
+                    case R.id.takephotos:
+                        //创建File对象，用于存储拍照后的图片,命名为outputimage.jpg,存放在SD卡应用关联缓存目录下
+                        File outputImage = new File(getExternalCacheDir(),"output_image.jpg");
+                        try {
+                            if (outputImage.exists()){
+                                outputImage.delete();
+                            }
+                            outputImage.createNewFile();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                        if (Build.VERSION.SDK_INT>=24){//安卓版本不低于7.0
+                            imageUri= FileProvider.getUriForFile(BackGroundActivity.this,"com.example.hebo.picturetest.fileprovider",outputImage);
+                        }else {
+                            imageUri=Uri.fromFile(outputImage);
+                        }
+                        //启动相机程序
+                        Intent intent=new Intent("android.media.action.IMAGE_CAPTURE");
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);//指定照片的输出地址
+                        startActivityForResult(intent,TAKE_PHOTO);
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+    //显示弹出式菜单
+    public void popupmenu(View v) {
+        popupMenu.show();
+    }
+
+    //打开图库
+    private void openAlbum(){
+        Intent intent=new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions,int[] grantResults){
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                }else {
+                    Toast.makeText(this,"你关闭了本项权限",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        switch (requestCode){
+            case CHOOSE_PHOTO://对图库中选择的图片进行处理
+                if (resultCode==RESULT_OK){
+                    handleImageOnKitkat(data);
+                }
+                break;
+            case TAKE_PHOTO://将拍摄的照片显示出来
+                if (resultCode==RESULT_OK){
+                    try {
+                        Bitmap bitmap= BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void handleImageOnKitkat(Intent data){
+        String imagePath=null;
+        Uri uri=data.getData();
+        if (DocumentsContract.isDocumentUri(this,uri)){
+            //如果是document类型的Uri，则通过document id 处理
+            String docId=DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id=docId.split(":")[1];//解析出数字格式的id
+                String selection=MediaStore.Images.Media._ID+"="+id;
+                imagePath=getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri= ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath=getImagePath(contentUri,null);
+            }
+        }else if ("content".equalsIgnoreCase(uri.getScheme())){
+            //如果是content类型的Uri，则使用普通方式处理
+            imagePath=getImagePath(uri,null);
+        }else if ("file".equalsIgnoreCase(uri.getScheme())){
+            //如果是file类型的Uri，直接获取图片路径即可
+            imagePath=uri.getPath();
+        }
+        displayImage(imagePath);//根据图片路径显示图片
+    }
+
+    private String getImagePath(Uri uri,String selection){
+        String path=null;
+        //通过Uri和selection来获取真实的图片路径
+        Cursor cursor=getContentResolver().query(uri,null,selection,null,null);
+        if (cursor!=null){
+            if (cursor.moveToFirst()){
+                path=cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void displayImage(String imagePath){
+        if (imagePath!=null){
+            Bitmap bitmap=BitmapFactory.decodeFile(imagePath);
+            picture.setImageBitmap(bitmap);
+        }else {
+            Toast.makeText(this,"获取图片失败",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/example/hebo/picturetest/MainActivity.java
+++ app/src/main/java/com/example/hebo/picturetest/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.hebo.picturetest;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "ChooseAreaFragment";
     private DrawerLayout mdrawerLayout;//滑动菜单
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +25,44 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);//添加toolBar
         mdrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
-        ActionBar actionBar=getSupportActionBar();
+        ActionBar actionBar=getSupportActionBar();//设置toolbar上的导航按钮
         if (actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+
+        NavigationView navigationView=(NavigationView)findViewById(R.id.nav_view);//获取滑动菜单实例
+        //navigationView.setItemIconTintList(null);//设置每个图标为原来的颜色
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                //在这里处理item的点击事件
+                item.setCheckable(true);//设置选项可选
+                switch (item.getItemId()){
+                    case R.id.backgroundpicture:
+                        Log.e(TAG, "显示背景图片");
+                        Intent back_intent=new Intent(MainActivity.this,BackGroundActivity.class);
+                        startActivity(back_intent);
+                        item.setCheckable(false);//设置选项不可选
+                        item.setChecked(false);
+                        break;
+                    case R.id.foregroundpicture:
+
+                        break;
+                    default:
+                }
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);//点击菜单之后滑动菜单收回
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
     }
 
     //toolbar菜单命令
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.toolbar,menu);
+        getMenuInflater().inflate(R.menu.nav_menu,menu);
         return true;
     }
     @Override
